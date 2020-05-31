26982871cbb082080e66a5ba5518610b5d10a3a9
gowtham6032 <38963408+gowtham6032@users.noreply.github.com>
2020-05-31 07:58:40 -0700

    Update set-region.sh

--- set-region.sh
+++ set-region.sh
@@ -1,3 +1,10 @@
-#!/bin/bash -x
-aws configure set region us-west-2
+#!/bin/bash
+mkdir -p /root/.aws
+touch /root/.aws/config
+FILE="/root/.aws/config"
+
+/bin/cat <<EOM >$FILE
+[default]
+region = us-west-2
+EOM
 echo "region set successfully"
